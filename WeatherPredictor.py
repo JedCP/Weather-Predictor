c51e258ce0ace6afd0fc7254e178a25ca5161cc2
JedCP <qjctpernecita@tip.edu.ph>
2024-05-19 21:43:17 +0800

    Update WeatherPredictor.py

--- WeatherPredictor.py
+++ WeatherPredictor.py
@@ -43,6 +43,6 @@ else:
     st.image(image, use_column_width=True)
     predict = import_and_predict(image, model)
     class_labels = ['Cloudy', 'Rain', 'Shine', 'Sunrise']
-    predicted_class_index = np.argmax(predict)
+    predicted_class_index = np.argmax(prediction)
     predicted_class_label = class_labels[predicted_class_index]
     st.success(f'Prediction: {predicted_class_label}')
