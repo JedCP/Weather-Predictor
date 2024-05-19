ff879239e50468baec24f5cca11487d1c4e1a239
JedCP <qjctpernecita@tip.edu.ph>
2024-05-19 22:16:45 +0800

    Update WeatherPredictor.py

--- WeatherPredictor.py
+++ WeatherPredictor.py
@@ -33,16 +33,16 @@ def import_and_predict(image_data, model):
     img_array = np.asarray(image)
     img_array = img_array[np.newaxis, ...]  # Add batch dimension
     img_array = img_array / 255.0  # Normalize to [0, 1] range
-    predict = (img_array)  # Example prediction
-    return predict
+    prediction = model.predict(img_array)  # Example prediction
+    return prediction
 
 if uploaded_file is None:
     st.text("Please upload an image file")
 else:
     image = Image.open(uploaded_file)
     st.image(image, use_column_width=True)
-    predict = import_and_predict(image, model)
+    prediction = import_and_predict(image, model)
     class_labels = ['Cloudy', 'Rain', 'Shine', 'Sunrise']
-    predicted_class_index = np.argmax(predict)
+    predicted_class_index = np.argmax(prediction)
     predicted_class_label = class_labels[predicted_class_index]
     st.success(f'Prediction: {predicted_class_label}')
