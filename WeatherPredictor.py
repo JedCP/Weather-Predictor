41edd55f079f6ba6ac0a5b53cd781a69d81127fc
JedCP <qjctpernecita@tip.edu.ph>
2024-05-19 14:53:41 +0800

    Add files via upload

--- WeatherPredictor.py
+++ WeatherPredictor.py
@@ -4,12 +4,12 @@ import streamlit as st
 import tensorflow as tf
 
 st.set_page_config(
-	page_title= "Weather-Predictor",
-	layout="centered",
-	inital_sidebar_state="expanded"
+    page_title="Weather-Predictor",
+    layout="centered",
+    initial_sidebar_state="expanded"
 )
 
 @st.cache.resource
 def load_model():
-	model = tf.keras.models.load_model("model_Weather.h5")
-	return model
+    model = tf.keras.models.load_model("model_Weather.h5")
+    return model
